c389f4a69b516784c0929771bc3babc8f044303b
Rodrigo Alvarado <alv16106@uvg.edu.gt>
2018-07-16 16:33:17 -0600

    textos de ganador y programacion defensiva

--- src/js/client.js
+++ src/js/client.js
@@ -2,22 +2,22 @@ const estado = {
   turno: 1,
   completado: 0,
   tamano: 3,
-  tablero: []
-}
+  tablero: [],
+};
 
-const imagenes = ['img/Equis.png', 'img/Circulo.png', 'img/Pepe.png']
+const imagenes = ['img/Equis.png', 'img/Circulo.png', 'img/Pepe.png', 'img/empate.png'];
 
-const setup = lSetup => {
-  //Inicia el array del juego
+const setup = (lSetup) => {
+  // Inicia el array del juego
   for (let i = 0; i < lSetup.tamano; i++) {
     lSetup.tablero[i] = [];
     for (let j = 0; j < lSetup.tamano; j++) {
       lSetup.tablero[i][j] = 2;
     }
   }
-}
+};
 
-const render = lRender => {
+const render = (lRender) => {
   if (root.hasChildNodes()) {
     root.innerHTML = null;
   }
@@ -33,15 +33,15 @@ const render = lRender => {
     lRender.completado = 0;
     setup(lRender);
     render(lRender);
-  }
+  };
 
   root.appendChild(title);
 
-  //Div principal, contiene la rejilla
+  // Div principal, contiene la rejilla
   const padre = document.createElement('div');
-  padre.className = 'padre'
+  padre.className = 'padre';
   root.appendChild(padre);
-  lRender.tablero.forEach(function(fila, numero){
+  lRender.tablero.forEach((fila, numero) => {
     const row = document.createElement('div');
     row.className = 'fila'
     padre.appendChild(row);
@@ -53,7 +53,7 @@ const render = lRender => {
       
       row.appendChild(espacio);
       espacio.onclick = () => {
-        if (lRender.tablero[numero][index] == 2) {
+        if (lRender.tablero[numero][index] == 2 && lRender.completado === 0) {
           lRender.tablero[numero][index] = lRender.turno%2;
           lRender.turno = lRender.turno + 1
           gano(lRender, numero, index);
@@ -62,59 +62,68 @@ const render = lRender => {
     };
     })
   });
-  root.appendChild(retryBtn);
-}
+  const sub = document.createElement('div');
+  sub.className = 'fila'
+  if (lRender.completado == 0) {
+    sub.innerHTML = `<h3>Es turno de &nbsp; &nbsp;</h3><img src= ${imagenes[lRender.turno%2]} class = 'turno'>` ;
+  }else if (lRender.completado == 3) {
+    sub.innerHTML = `<h3>Empate &nbsp; &nbsp;</h3><img src= ${imagenes[3]} class = 'turno'>` ;
+  }else{
+    sub.innerHTML = `<h3>El ganador es: &nbsp; &nbsp;</h3><img src= ${imagenes[(lRender.turno+1)%2]} class = 'turno'>` ;
+  }
+
+  padre.appendChild(sub)
+  padre.appendChild(retryBtn);
+};
 
 const gano = (estado, x, y) => {
-  //Check columna
-  for(let i = 0; i < estado.tamano; i++){
-    if(estado.tablero[i][y] == estado.turno%2 || estado.tablero[i][y] == 2){
+  // Check columna
+  for (let i = 0; i < estado.tamano; i++) {
+    if (estado.tablero[i][y] == estado.turno % 2 || estado.tablero[i][y] == 2) {
       break;
     }
-    if(i == estado.tamano-1){
-        console.log(`Gano el jugador ${estado.turno%2 + 1}`); 
+    if (i == estado.tamano - 1) {
+      estado.completado = estado.turno % 2 + 1;
     }
   }
 
-  //Check row
-  for(let i = 0; i < estado.tamano; i++){
-    if(estado.tablero[x][i] == estado.turno%2 || estado.tablero[x][i] == 2){
+  // Check row
+  for (let i = 0; i < estado.tamano; i++) {
+    if (estado.tablero[x][i] == estado.turno % 2 || estado.tablero[x][i] == 2) {
       break;
     }
-    if(i == estado.tamano-1){
-        console.log(`Gano el jugador ${estado.turno%2 + 1}`); 
+    if (i == estado.tamano - 1) {
+      estado.completado = estado.turno % 2 + 1;
     }
   }
 
-  //check diagonal
-  if (x==y){
-    for(let i = 0; i < estado.tamano; i++){
-      if(estado.tablero[i][i] == estado.turno%2 || estado.tablero[i][i] == 2){
+  // check diagonal
+  if (x == y) {
+    for (let i = 0; i < estado.tamano; i++) {
+      if (estado.tablero[i][i] == estado.turno % 2 || estado.tablero[i][i] == 2) {
         break;
       }
-      if(i == estado.tamano-1){
-          console.log(`Gano el jugador ${estado.turno%2 + 1}`);
+      if (i == estado.tamano - 1) {
+        estado.completado = estado.turno % 2 + 1;
       }
     }
   }
 
-  if (x == estado.tamano -1 - y) {
-    for(let i = estado.tamano - 1; i > -1; i--){
-      if(estado.tablero[estado.tamano - 1 - i][i] == estado.turno%2 || estado.tablero[estado.tamano - 1 - i][i] == 2){
+  if (x == estado.tamano - 1 - y) {
+    for (let i = estado.tamano - 1; i > -1; i--) {
+      if (estado.tablero[estado.tamano - 1 - i][i] == estado.turno % 2 || estado.tablero[estado.tamano - 1 - i][i] == 2) {
         break;
       }
-      if(i == 0){
-          console.log(`Gano el jugador ${estado.turno%2 + 1}`); 
+      if (i == 0) {
+        estado.completado = estado.turno % 2 + 1;
       }
     }
-    
   }
 
   if (estado.turno == (estado.tamano * estado.tamano) + 1) {
-    console.log("empate");
+    estado.completado = 3;
   }
-  
-}
+};
 
 setup(estado);
 render(estado);
